d7b122b27264aff9a6fb1d18a43108af8291202c
Brucetran323 <brucetran323@gmail.com>
2023-04-02 21:59:44 -0400

    done more

--- app/pokemon/routes.py
+++ app/pokemon/routes.py
@@ -8,7 +8,15 @@ pokemon = Blueprint('pokemon', __name__, template_folder='pokemon_templates')
 
 @pokemon.route('/catch-em/<int:p_id>')
 def catchPokemon(p_id):
-    poke = Pokemon.query.filter_by(poke_id=p_id).first()
-    flash(f"{poke} has been caught!")
-    current_user.catchPokemon(poke)
-    return redirect(url_for('homePage'))
+    current_user_poke_count = len(current_user.caught.all())
+    if current_user_poke_count >= 5:
+        print("NO!!!!")
+        return redirect(url_for('homePage'))
+    else:
+        poke = Pokemon.query.filter_by(poke_id=p_id).first()
+        flash(f"{poke} has been caught!")
+        current_user.catchPokemon(poke)
+        pokemon_info = Pokemon.query.filter_by(poke_id=p_id).first()
+        print(pokemon_info)
+        return render_template('catch-me.html', pokemon = pokemon_info)
+    
